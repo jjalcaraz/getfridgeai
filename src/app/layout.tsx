5c5c653d50d15c97e28a399e50df50aa268dac99
Juan J Alcaraz <54329769+jjalcaraz@users.noreply.github.com>
2026-08-03 07:30:00 -0500

    Add Google Tag Manager (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -53,6 +54,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <GoogleTagManager gtmId="GTM-WLJ4SXP6" />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
